6fc1111b326562e03ca7b5be86d9ae6e94af94e0
Jhonatan Rodriguez <70608790+Jhrodriguezi@users.noreply.github.com>
2022-06-06 17:33:09 +0000

    vista de consullta, conexion front y back. Funciones de log agregadas

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -1,32 +1,125 @@
 const { Movie, functions_movie } = require('../models/movie');
-const {Log, functions_log} = require('../models/log');
+const { Log, functions_log } = require('../models/log');
+const { Review, functions_review } = require('../models/review');
+
+async function checkData(movie) {
+  if (!movie.title) {
+    movie.title = "??";
+  }
+
+  if (movie.poster_path) {
+    movie.poster_path = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  } else {
+    movie.poster_path = "/resources/img/imagen_no_disponible.png";
+  }
+  if (!movie.original_language) {
+    movie.original_language = "-";
+  }
+
+  if (movie.genres) {
+    let string = "";
+    let flag = true;
+    movie.genres.forEach(genre => {
+      if (flag) {
+        string += genre.name;
+        flag = false;
+      } else {
+        string += ", " + genre.name;
+      }
+    })
+    movie.genres = string;
+  } else {
+    movie.genres = "-";
+  }
+
+  if (!movie.original_title) {
+    movie.original_title = "-";
+  }
+
+  if (!movie.release_date) {
+    movie.release_date = "-";
+  }
+
+  let average;
+
+  try {
+    average = await functions_review.getAverageMovie(movie.id);
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (!average) {
+    average = "-";
+  }
+
+  movie.vote_average = average;
+  if (!movie.num_reviews) {
+    movie.num_reviews = 0;
+  }
+
+  if (!movie.overview) {
+    movie.overview = "-";
+  }
+
+  return movie;
+}
+
 
 const movie_functions_movie = {
   cargarPelicula: async (req, res) => {
-    let movie, newLog;
-    try{
+    let movie = {}, newLog, reviews = [];
+    try {
       movie = await functions_movie.peliculaById(req.query.id);
-      newLog = new Log(null, req.session.name, null, "Read movie", movie.id, 'El usuario ha consultado la pelicula: '+movie.title);
-      await functions_log.insertLog(newLog);
-    }catch(e){
+      reviews = await functions_review.getAllReviewsByIdMovie(req.query.id);
+      movie.num_reviews = reviews.length;
+      movie = await checkData(movie);
+    } catch (e) {
       console.log(e);
     }
-    res.send(movie);
+    if (req.session.loggedin) {
+      try{
+        newLog = new Log(null, req.session.name, null, "Read movie", movie.id, 'El usuario ha consultado la pelicula: ' + movie.title);
+        await functions_log.insertLog(newLog);
+      }catch(e){
+        console.log(e);
+      }
+      res.render("movie", {
+        login: true,
+        name: req.session.name,
+        movie: movie,
+        reviews: reviews
+      });
+    } else {
+      res.render("movie", {
+        login: false,
+        name: "??",
+        alert: true,
+        alertTitle: "Login requerido",
+        alertMessage: "Debe identificarse para acceder a la pagina",
+        alertIcon: 'info',
+        showConfirmButton: true,
+        timer: false,
+        ruta: '',
+        movie: movie,
+        reviews: reviews
+      });
+    }
+    //res.send(movie);
   },
   obtenerPeliculasPorNombre: async (req, res) => {
     let movies;
-    try{
+    try {
       movies = await functions_movie.buscar(req.query.name);
-    }catch(e){
+    } catch (e) {
       console.log(e);
     }
     res.send(movies);
   },
   obtenerPeliculasPopulares: async (req, res) => {
     let movies;
-    try{
+    try {
       movies = await functions_movie.masPopulares();
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
     res.send(movies);
--- models/review.js
+++ models/review.js
@@ -11,6 +11,130 @@ class Review {
   #puntuacion;
   #idusuario;
   #idpelicula;
+
+  constructor(id, encabezado, contenido, likes, dislikes, denuncias, fechapub, puntuacion, idusuario, idpelicula){
+    this.id = id;
+    this.encabezado = encabezado;
+    this.contenido = contenido;
+    this.likes = likes;
+    this.dislikes = dislikes;
+    this.denuncias = denuncias;
+    this.fechapub = fechapub;
+    this.puntuacion = puntuacion;
+    this.idusuario = idusuario;
+    this.idpelicula = idpelicula;
+  }
+
+  get getId() {
+    return this.id;
+  }
+
+  /**
+     * @param {any} id
+     */
+  set setId(id) {
+    this.id = id;
+  }
+
+  get getEncabezado() {
+    return this.encabezado;
+  }
+
+  /**
+     * @param {any} encabezado
+     */
+  set setEncabezado(encabezado) {
+    this.encabezado = encabezado;
+  }
+
+  get getContenido() {
+    return this.contenido;
+  }
+
+  /**
+     * @param {any} contenido
+     */
+  set setContenido(contenido) {
+    this.contenido = contenido;
+  }
+
+  get getLikes() {
+    return this.likes;
+  }
+
+  /**
+     * @param {any} likes
+     */
+  set setLikes(likes) {
+    this.likes = likes;
+  }
+
+  get getDislikes() {
+    return this.dislikes;
+  }
+
+  /**
+     * @param {any} dislikes
+     */
+  set setDislikes(dislikes) {
+    this.dislikes = dislikes;
+  }
+
+  get getDenuncias() {
+    return this.denuncias;
+  }
+
+  /**
+     * @param {any} denuncias
+     */
+  set setDenuncias(denuncias) {
+    this.denuncias = denuncias;
+  }
+
+  get getFechapub() {
+    return this.fechapub;
+  }
+
+  /**
+     * @param {any} fechapub
+     */
+  set setFechapub(fechapub) {
+    this.fechapub = fechapub;
+  }
+
+  get getPuntuacion() {
+    return this.puntuacion;
+  }
+
+  /**
+     * @param {any} puntuacion
+     */
+  set setPuntuacion(puntuacion) {
+    this.puntuacion = puntuacion;
+  }
+
+  get getIdUsuario() {
+    return this.idusuario;
+  }
+
+  /**
+     * @param {any} idusuario
+     */
+  set setIdUsuario(idusuario) {
+    this.idusuario = idusuario;
+  }
+
+  get getIdPelicula() {
+    return this.idpelicula;
+  }
+
+  /**
+     * @param {any} idpelicula
+     */
+  set setIdPelicula(idpelicula) {
+    this.idpelicula = idpelicula;
+  }
+  
 }
 
 const functions_review = {
@@ -22,6 +146,39 @@ const functions_review = {
       let result = await client.query(sql, values);
       console.log(result);
       client.release(true);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  getAllReviewsByIdMovie: async (idpelicula)=>{
+    try {
+      let client = await connection.connect();
+      let sql = "SELECT * FROM resena WHERE idpelicula=$1";
+      let values = [idpelicula];
+      let result = [], resp;
+      resp = await client.query(sql, values);
+      resp.rows.forEach(element => {
+        result.push(new Review(element.id, element.encabezado, element.contenido, element.likes, element.dislikes, element.denuncias, element.fechapub, element.puntuacion, element.idusuario, element.idpelicula));
+      });
+      client.release(true);
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  //temporal, el promedio deberia guardarse en la bd en la tabla movie.
+  getAverageMovie: async (idpelicula)=>{
+    try {
+      let client = await connection.connect();
+      let sql = "SELECT avg(puntuacion) as average FROM resena WHERE idpelicula=$1";
+      let values = [idpelicula];
+      let result, resp;
+      resp = await client.query(sql, values);
+      resp.rows.forEach(element => {
+        result= element.average;
+      });
+      client.release(true);
+      return result;
     } catch (e) {
       console.log(e);
     }
